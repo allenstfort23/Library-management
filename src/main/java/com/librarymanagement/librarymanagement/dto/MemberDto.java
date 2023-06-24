d0010c6ce8f2ad91d80718ae8ca08c85c50476a1
allensftort <allenstfort1@gmail.com>
2023-06-24 01:19:04 -0500

    Library project set up

---
+++ src/main/java/com/librarymanagement/librarymanagement/dto/MemberDto.java
@@ -0,0 +1,15 @@
+package com.librarymanagement.librarymanagement.dto;
+
+
+import java.time.LocalDateTime;
+
+
+public class MemberDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String memberId;
+    private String book;
+    private LocalDateTime createdOn;
+    private LocalDateTime updateOn;
+}
---
+++ src/main/java/com/librarymanagement/librarymanagement/model/Member.java
@@ -0,0 +1,28 @@
+package com.librarymanagement.librarymanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String memberId;
+    private String book;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updateOn;
+}
